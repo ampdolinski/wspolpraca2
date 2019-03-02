042c30f18473dc49ddd6f0cf65b4dccea594621b
Adam Doliński <a.m.p.dolinski@gmail.com>
2019-03-02 14:46:38 +0100

    aktualizacja maina

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,37 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Nowe podejście!");
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj, co chcesz zrobić:");
         System.out.println("(add/sub/div/mul)");
-        
+
+        String dzialanie = scanner.nextInt();
+
+//        String dzialanie = scanner.nextInt();
+
+        double x;
+        double y;
+
+        System.out.println("Podaj pierwszą liczbę:");
+        x = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Podaj drugą liczbę:");
+        y = Double.parseDouble(scanner.nextLine());
+
+
+        do {
+            switch (dzialanie){
+                case "add":
+
+            }
+
+
+
+        } while ("quit");
+
 
     }
 }
