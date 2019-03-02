3edd2749f982b72562b74cdc22a52d841fbfe960
Jacek Herrmann <jacek.herrmann@gmail.com>
2019-03-02 15:16:24 +0100

    Zmiany

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,43 +3,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double x;
-        double y;
         boolean loop = true;
 
         while (loop) {
-            System.out.print("Podaj pierwszą liczbę:");
-            x = Double.parseDouble(scanner.nextLine());
+            try {
 
-            System.out.print("Podaj drugą liczbę:");
-            y = Double.parseDouble(scanner.nextLine());
-
-            System.out.println("Podaj, co chcesz zrobić (add / sub / div / mul / quit):");
-            switch (scanner.nextLine().toLowerCase()) {
-                case "add":
-                    SumEquation suma = new SumEquation(x, y);
-                    System.out.println(suma.calculate());
-                    break;
-                case "sub":
-                    SubtractEquation rownica = new SubtractEquation(x, y);
-                    System.out.println(rownica.calculate());
-                    break;
-                case "div":
-                    DivEquation dzielenie = new DivEquation(x, y);
-                    System.out.println(dzielenie.calculate());
-                    break;
-                case "mul":
-                    MulEquation mnozenie = new MulEquation(x, y);
-                    System.out.println(mnozenie.calculate());
-                    break;
-                case "quit":
-                    loop = false;
-                    break;
-                default:
-                    System.out.println("Nie ma takiej operacji, spóbuj jeszcze raz!");
-                    break;
+                System.out.println("Podaj, co chcesz zrobić (add / sub / div / mul / quit):");
+                switch (scanner.nextLine().toLowerCase()) {
+                    case "add":
+                        SumEquation suma = new SumEquation(getNumX(), getNumY());
+                        System.out.println(suma.calculate());
+                        break;
+                    case "sub":
+                        SubtractEquation rownica = new SubtractEquation(getNumY(), getNumY());
+                        System.out.println(rownica.calculate());
+                        break;
+                    case "div":
+                        DivEquation dzielenie = new DivEquation(getNumX(), getNumY());
+                        System.out.println(dzielenie.calculate());
+                        break;
+                    case "mul":
+                        MulEquation mnozenie = new MulEquation(getNumX(), getNumY());
+                        System.out.println(mnozenie.calculate());
+                        break;
+                    case "quit":
+                        loop = false;
+                        break;
+                    default:
+                        System.out.println("Nie ma takiej operacji, spóbuj jeszcze raz!");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Niepoprawnie wprowadzona wartość spróbuj jeszcze raz!");
             }
         }
         System.out.println("KONIEC");
+    }
+
+    private static double getNumX() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj pierwszą liczbę:");
+        return Double.parseDouble(scan.nextLine());
+    }
+
+    private static double getNumY() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj drugą liczbę:");
+        return Double.parseDouble(scan.nextLine());
     }
 }
